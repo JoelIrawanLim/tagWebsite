32efeae4dba36c7db28547a72f6d3c044c654af8
Joel <joelirawanlim@gmail.com>
2025-03-03 23:59:19 +0800

    Revert "Update main.tsx"
    This reverts commit 98e630a8cb382e8121d0970ca1ea95f5043c7414.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'; 
 import './index.css'
-import Links from './applinks';
+import Links from './links';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
