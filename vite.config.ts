f157f9369a12383554bf5745c6ae43e4f51b6384
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2025-03-03 20:06:07 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+  allowedHosts: ['4631fecf-f464-416b-a18d-4ed314877021-00-385v3475c2h8d.pike.replit.dev'],
+  }
 })
