9003c9a83e76989519cabee89d96cc228d8ec955
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2025-03-04 23:47:44 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Links from './applinks';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Links />
   </StrictMode>,
 )
